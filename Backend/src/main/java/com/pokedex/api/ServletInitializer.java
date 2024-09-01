bb875b8c66f48252f7757be416ca639478149295
JimgleBell <gabrymedeiros7@gmail.com>
2024-09-01 13:01:44 -0300

    creating the springboot

---
+++ Backend/src/main/java/com/pokedex/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Pokedex.Backend.src.main.java.com.pokedex.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder
+  configure(SpringApplicationBuilder application) {
+    return application.sources(ApiApplication.class);
+  }
+}
---
+++ Backend/src/test/java/com/pokedex/api/ApiApplicationTests.java
@@ -0,0 +1,11 @@
+package Pokedex.Backend.src.test.java.com.pokedex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
