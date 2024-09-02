1b290a2a16345fca555b81241625a727a1bb3df2
JimgleBell <gabrymedeiros7@gmail.com>
2024-09-02 09:20:14 -0300

    test

--- Backend/pokedex-api/src/main/java/com/pokedex/pokedex_api/PokedexController.java
+++ Backend/pokedex-api/src/main/java/com/pokedex/pokedex_api/PokedexController.java
@@ -5,21 +5,24 @@ import java.util.ArrayList;
 import com.pokedex.pokedex_api.entities.PokemonEntity;
 
 public class PokedexController {
-    private ArrayList<PokemonEntity> pokedex = new  ArrayList<>();
-    public PokedexController(){
-        //Essa parte pegamos os dados do pokeApi, por enquanto só vou fazer com 2 exemplos
-        PokemonEntity pokemon_1= new PokemonEntity("Bulbasaur", 1);
-        PokemonEntity pokemon_2= new PokemonEntity("Ivysaur", 2);
+    private ArrayList<PokemonEntity> pokedex = new ArrayList<>();
+
+    public PokedexController() {
+        // Essa parte pegamos os dados do pokeApi, por enquanto só vou fazer com 2
+        // exemplos
+        PokemonEntity pokemon_1 = new PokemonEntity("Bulbasaur", 1);
+        PokemonEntity pokemon_2 = new PokemonEntity("Ivysaur", 2);
         pokedex.add(pokemon_1);
         pokedex.add(pokemon_2);
     }
+
     public ArrayList<PokemonEntity> getAll() {
         return pokedex;
     }
 
     public PokemonEntity getPokemonById(Integer id) {
 
-        for(PokemonEntity p: pokedex) {
+        for (PokemonEntity p : pokedex) {
             if (p.getId().equals(id)) {
                 return p;
             }
