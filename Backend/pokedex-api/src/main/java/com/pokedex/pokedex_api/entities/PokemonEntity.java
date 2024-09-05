d27d3063325d400771282e354366d1bad6e3d83f
Eryancosta <eryan.costa@hotmail.com>
2024-09-05 15:56:59 -0300

    s

--- Backend/pokedex-api/src/main/java/com/pokedex/pokedex_api/entities/PokemonEntity.java
+++ Backend/pokedex-api/src/main/java/com/pokedex/pokedex_api/entities/PokemonEntity.java
@@ -3,10 +3,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class PokemonEntity {
 private String name;
+private String species;
+
+    @Id
     Integer id;
     ArrayList<String> types;
     ArrayList<String> evolves_to;
